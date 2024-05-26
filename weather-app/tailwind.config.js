d812a6f1287d32b4efa3c5fde735c88b74c3ad6f
Jessie <yujiaxin2009@gmail.com>
2024-05-26 17:02:07 +1000

    add fonts

--- weather-app/tailwind.config.js
+++ weather-app/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'alimama': ['AlimamaFangYuanTiVF-Square', 'helvetica']
+      }
+    },
   },
   plugins: [],
 }
